fce67d2d17119c9fce4e47ebefd52fd025e540f5
daniorio <danil_lopin@mail.ru>
2020-03-29 16:31:12 +0300

    Java check

--- src/main/java/ml/luxinfine/loader/Config.java
+++ src/main/java/ml/luxinfine/loader/Config.java
@@ -1,6 +1,8 @@
 package ml.luxinfine.loader;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.sun.javafx.PlatformUtil.isWindows;
 
@@ -9,6 +11,12 @@ class Config {
     static final String launcherName = "LuxinfineLauncher";
     static final String path = appdataPath + File.separator + ".Luxinfine" + File.separator + "updates";;
     static final String fullpath = path + File.separator + launcherName + getFileExtension();
-    static final String url = "https://luxinfine.ml/" + launcherName + getFileExtension();
+    static final List<String> urls = Arrays.asList("https://luxinfine.ml/" + launcherName + getFileExtension(),
+                                                    "http://luxinfine.ml:82/updates/" + launcherName + getFileExtension(),
+                                                    "http://luxinfine.ml/updates/" + launcherName + getFileExtension(),
+                                                    "https://luxinfine.ml/updates/" + launcherName + getFileExtension(),
+                                                    "https://luxinfine.ml:444/updates/" + launcherName + getFileExtension());
     static String getFileExtension() { if(isWindows()) { return ".exe"; } else { return ".jar"; } }
+    static final String javaurl = "https://www.java.com/ru/download/manual.jsp";
+    static final int minJavaVersion = 241;
 }
--- src/main/java/ml/luxinfine/loader/Main.java
+++ src/main/java/ml/luxinfine/loader/Main.java
@@ -1,26 +1,51 @@
 package ml.luxinfine.loader;
 
+import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
 import java.net.URL;
 import java.nio.channels.Channels;
+import java.util.ArrayList;
 
 class Main {
-    private static String url = Config.url;
     private static String path = Config.path;
     private static String fullpath = Config.fullpath;
+    private static boolean isLoaded = false;
 
     public static void main(String[] args) throws IOException {
-        if (new File(fullpath).exists()) {
-            Desktop.getDesktop().open(new File(fullpath));
-        } else {
-            try {
-                new File(path).mkdirs();
-                new FileOutputStream(fullpath).getChannel().transferFrom(Channels.newChannel(new URL(url).openStream()), 0, Long.MAX_VALUE);
-                Desktop.getDesktop().open(new File(fullpath));
-            } catch (IOException e) { e.printStackTrace(); }
+        if (!new File(fullpath).exists() || new File(fullpath).length() == 0) {
+            new File(path).mkdirs();
+            for (String url : Config.urls) {
+                try { new FileOutputStream(fullpath).getChannel().transferFrom(Channels.newChannel(new URL(url).openStream()), 0, Long.MAX_VALUE); } catch (Exception e) { continue; }
+                isLoaded = true;
+                break;
+            }
+        } else isLoaded = true;
+        //* Проверка загруженного файла *//
+        if(!isLoaded) setError("Не удалось загрузить файл лаунчера");
+        //* Проверка битности java *//
+        StringBuilder answer = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("java -version").getInputStream()));
+        while ((br.readLine())!=null) answer.append(br.readLine());
+        if(!answer.toString().toLowerCase().contains("64-bit") && System.getProperty("os.arch").equals("64")) setInfo("Отсутствует java нужной разрядности");
+        //* Проверка версии java *//
+        if(Integer.parseInt(System.getProperty("java.version").split("_")[1]) < Config.minJavaVersion) setInfo("Отсутствует java нужной версии");
+        Desktop.getDesktop().open(new File(fullpath));
+    }
+
+    private static void setError(String msg) throws IOException {
+        JOptionPane.showMessageDialog(null, msg, "Ошибка!", JOptionPane.ERROR_MESSAGE);
+        System.exit(0);
+    }
+
+    private static void setInfo(String msg) throws IOException {
+        int selection = JOptionPane.showConfirmDialog(null, "Перейти на сайт для установки?", msg, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (selection == JOptionPane.YES_OPTION) {
+            Desktop.getDesktop().browse(URI.create(Config.javaurl));
+            System.exit(0);
         }
     }
+
+
 }
