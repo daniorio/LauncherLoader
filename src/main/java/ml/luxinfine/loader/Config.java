9b1bd55e674e144b8a5066818d5151f73378d2f8
daniorio <danil_lopin@mail.ru>
2020-04-16 14:30:03 +0300

    Auto java downloading

--- src/main/java/ml/luxinfine/loader/Config.java
+++ src/main/java/ml/luxinfine/loader/Config.java
@@ -7,8 +7,11 @@ import java.util.List;
 import static ml.luxinfine.loader.Main.getFileExtension;
 
 class Config {
+    //*    Название файла лаунчера    *//
     static final String launcherName = "LuxinfineLauncher";
+    //*    Путь к месту хранения файла лаунчера    *//
     static final String path = System.getenv("APPDATA") + File.separator + ".Luxinfine" + File.separator + "updates";
+    //*    Можно указать список ссылок откуда качается лаунчер, можно одну    *//
     static final List<String> urls = Arrays.asList(
                                                     "http://luxinfine.ml:82/updates/" + launcherName + getFileExtension(),
                                                     "https://luxinfine.ml/" + launcherName + getFileExtension(),
@@ -16,6 +19,10 @@ class Config {
                                                     "https://luxinfine.ml/updates/" + launcherName + getFileExtension(),
                                                     "https://luxinfine.ml:444/updates/" + launcherName + getFileExtension()
                                                     );
+    //*    Ссылки для загрузки джавы    *//
     static final String javaurl = "https://www.java.com/ru/download/manual.jsp";
-    static final int minJavaVersion = 241;
+    static final String javax64 = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=242060_3d5a2bb8f8d4428bbe94aed7ec7ae784";
+    static final String javax32 = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=242058_3d5a2bb8f8d4428bbe94aed7ec7ae784";
+    //*    Минимальная версия джавы    *//
+    static final int minJavaVersion = 251;
 }
--- src/main/java/ml/luxinfine/loader/Main.java
+++ src/main/java/ml/luxinfine/loader/Main.java
@@ -28,25 +28,37 @@ class Main {
         if(!new File(fullpath).canRead()) setError("Не удалось прочитать файл лаунчера");
         if(!new File(fullpath).canExecute()) setError("Не удалось запустить файл лаунчера");
         //* Проверка битности java *//
-        StringBuilder answer = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("java -version").getInputStream()));
+        final StringBuilder answer = new StringBuilder();
+        final BufferedReader br = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("java -version").getInputStream()));
         while ((br.readLine())!=null) answer.append(br.readLine());
-        if(!answer.toString().toLowerCase().contains("64-bit") && System.getProperty("os.arch").equals("64")) setInfo("Отсутствует java нужной разрядности");
+        if(!answer.toString().toLowerCase().contains("64-bit") && System.getProperty("os.arch").equals("64")) setInfo("Отсутствует java нужной разрядности", "Установить сейчас?");
         //* Проверка версии java *//
-        if(Integer.parseInt(System.getProperty("java.version").split("_")[1]) < Config.minJavaVersion) setInfo("Отсутствует java нужной версии");
-        Runtime.getRuntime().exec("java -jar " + fullpath);
+        if(Integer.parseInt(System.getProperty("java.version").split("_")[1]) < Config.minJavaVersion) setInfo("Отсутствует java нужной версии", "Установить сейчас?");
+        try { Runtime.getRuntime().exec("java -jar " + fullpath); } catch (Exception e) { setError("Не удалось запустить файл лаунчера"); }
         //Desktop.getDesktop().open(new File(fullpath));
     }
 
-    private static void setError(String msg) throws IOException {
+    private static void setError(String msg) {
         JOptionPane.showMessageDialog(null, msg, "Ошибка!", JOptionPane.ERROR_MESSAGE);
         System.exit(0);
     }
 
-    private static void setInfo(String msg) throws IOException {
-        int selection = JOptionPane.showConfirmDialog(null, "Перейти на сайт для установки?", msg, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+    private static void setInfo(String msg1, String msg2) throws IOException {
+        final int selection = JOptionPane.showConfirmDialog(null, msg2, msg1, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
         if (selection == JOptionPane.YES_OPTION) {
-            Desktop.getDesktop().browse(URI.create(Config.javaurl));
+            if(isWindows()) {
+                final String url = System.getProperty("os.arch").equals("64") ? Config.javax64 : Config.javax32;
+                final String filepath = System.getProperty("user.dir") + File.separator + "javainstaller.exe";
+                JOptionPane.showConfirmDialog(null, "Сейчас начнется установка джавы, пожалуйста, подождите пару минут", "", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE);
+                FileOutputStream fos = new FileOutputStream(filepath);
+                fos.getChannel().transferFrom(Channels.newChannel(new URL(url).openStream()), 0, Long.MAX_VALUE);
+                fos.close();
+                if(!new File(filepath).exists()) setError("Не удалось загрузить установщик джавы");
+                if(!new File(filepath).canRead()) setError("Не удалось прочитать установщик джавы");
+                if(!new File(filepath).canExecute()) setError("Не удалось запустить установщик джавы");
+                JOptionPane.showConfirmDialog(null, "Продолжите установку в ручном режиме, а после заного запустить лаунечер", "", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE);
+                Desktop.getDesktop().open(new File(filepath));
+            } else Desktop.getDesktop().browse(URI.create(Config.javaurl));
             System.exit(0);
         }
     }
